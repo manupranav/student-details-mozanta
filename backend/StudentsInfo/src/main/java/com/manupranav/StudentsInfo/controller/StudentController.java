17836183dac0e6243f37bf3dc50363e94bc13a8f
Manu Pranav <manupranav62@gmail.com>
2023-06-15 19:58:24 +0530

    added controller

---
+++ backend/StudentsInfo/src/main/java/com/manupranav/StudentsInfo/controller/StudentController.java
@@ -0,0 +1,28 @@
+package com.manupranav.StudentsInfo.controller;
+
+import com.manupranav.StudentsInfo.model.Student;
+import com.manupranav.StudentsInfo.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/")
+public class StudentController {
+    @Autowired
+    public StudentService service;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student createStudent(@RequestBody Student student){
+        return service.addStudent(student);
+
+    }
+
+    @GetMapping
+    public List<Student> getAllStudents(){
+        return service.findAllStudents();
+    }
+}
