92a7abd09dc7512152b34422904d69e2037a5cf7
Manu Pranav <manupranav62@gmail.com>
2023-06-15 19:57:35 +0530

    added backend

---
+++ backend/StudentsInfo/src/test/java/com/manupranav/StudentsInfo/StudentsInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manupranav.StudentsInfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
